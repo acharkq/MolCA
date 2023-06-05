26a24991968a4d2701ebc1f30f2281d6fce17ee8
zyliu <acharkq@gmail.com>
2023-06-05 15:42:15 +0000

    update

---
+++ GraphTextPretrain/data_provider/smiles_caption_dataset.py
@@ -0,0 +1,66 @@
+import torch
+from torch_geometric.data import Dataset
+import os
+
+class SmilesCaption(Dataset):
+    def __init__(self, root, text_max_len, prompt=None):
+        super(SmilesCaption, self).__init__(root)
+        self.root = root
+        self.text_max_len = text_max_len
+        # self.graph_name_list = os.listdir(root+'graph/')
+        # self.graph_name_list.sort()
+        self.text_name_list = os.listdir(root+'text/')
+        self.text_name_list.sort()
+        self.smiles_name_list = os.listdir(root+'smiles/')
+        self.smiles_name_list.sort()
+        self.tokenizer = None
+        
+        if not prompt:
+            self.prompt = 'The SMILES of this molecule is [START_SMILES]{}[END_SMILES]. '
+        else:
+            self.prompt = prompt
+
+    def get(self, index):
+        return self.__getitem__(index)
+
+    def len(self):
+        return len(self)
+
+    def __len__(self):
+        return len(self.text_name_list)
+
+    def __getitem__(self, index):
+        text_name = self.text_name_list[index]
+        smiles_name = self.smiles_name_list[index]
+
+        # load and process text
+        text_path = os.path.join(self.root, 'text', text_name)
+        
+        text_list = []
+        count = 0
+        for line in open(text_path, 'r', encoding='utf-8'):
+            count += 1
+            text_list.append(line.strip('\n'))
+            if count > 100:
+                break
+        text = ' '.join(text_list) + '\n'
+
+        # load and process smiles
+        smiles_path = os.path.join(self.root, 'smiles', smiles_name)
+        with open(smiles_path, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            assert len(lines) == 1
+            smiles = lines[0].strip()
+
+        smiles_prompt = self.prompt.format(smiles)
+        return smiles_prompt, text
+    
+    def tokenizer_text(self, text):
+        sentence_token = self.tokenizer(text=text,
+                                        truncation=True,
+                                        padding='max_length',
+                                        add_special_tokens=True,
+                                        max_length=self.text_max_len,
+                                        return_tensors='pt',
+                                        return_attention_mask=True)
+        return sentence_token
