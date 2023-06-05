2d336d07045656cbcce612472fedbcc5c3b3a881
Liu <e0517239@asp2a-login-nus02.head.cm.asp2a.nscc.sg>
2023-06-05 11:50:40 +0800

    update

--- GraphTextPretrain/scripts/stage2_vicuna.sh
+++ GraphTextPretrain/scripts/stage2_vicuna.sh
@@ -1,21 +1,20 @@
 {
-filename='llama13b';
-llm="./llms/vicuna-13b";
-devices='6,7';
+source /home/users/nus/e0517239/scratch/anaconda3/etc/profile.d/conda.sh;
+conda activate pth20;
+cd /home/project/11002701/zyliu/Mol-BLIP2/GraphTextPretrain;
+
+filename='vicuna7b';
+llm="./llms/vicuna-7b";
+devices='0,1';
 
 # pretrain on stage1 checkpoint
-# python stage2_vicuna.py --devices $devices --filename "pt_${filename}" --stage1_path "all_checkpoints/mola_dataset_notune_gnn_nosampline/epoch=49-step=120950.ckpt" --opt_model $llm --max_epochs 10 --mode eval --prompt 'The molecule\t' --tune_gnn;
+python stage2.py --devices $devices --filename "pt_${filename}" --stage1_path "all_checkpoints/stage1_default/epoch=49-step=120950.ckpt" --opt_model $llm --max_epochs 10 --mode pretrain --prompt 'The molecule\t' --tune_gnn --batch_size 16 --accumulate_grad_batches 2;
 
-# python stage2_vicuna.py --devices $devices --filename "pt_${filename}" --stage2_path "all_checkpoints/pt_llama13b/last.ckpt" --opt_model $llm --max_epochs 10 --mode eval --prompt 'The molecule\t' --tune_gnn;
-
-
-# # fine-tune on the pretrain's checkpoint
-# python pretrain_stage2.py --devices $devices --filename "ft_${filename}" --stage2_path "all_checkpoints/pt_${filename}/last.ckpt" --opt_model $llm --max_epochs 10 --mode ft --tune_gnn;
 
 # fine-tune on the pretrain's checkpoint
 # python stage2_vicuna.py --devices $devices --filename "ft_${filename}_lora_tuning" --stage2_path "all_checkpoints/pt_${filename}/last.ckpt" --opt_model $llm --max_epochs 100 --mode ft --tune_gnn --prompt 'The molecule\t' --init_lr 1e-4 --scheduler None --lora_tuning --batch_size 8 --accumulate_grad_batches 4;
 
 # fine-tune on the pretrain's checkpoint without lora
-python stage2_vicuna.py --devices $devices --filename "ft_${filename}" --stage2_path "all_checkpoints/pt_${filename}/last.ckpt" --opt_model $llm --max_epochs 100 --mode ft --tune_gnn --prompt 'The molecule\t' --init_lr 1e-4 --scheduler None --batch_size 8 --accumulate_grad_batches 4;
+python stage2.py --devices $devices --filename "ft_${filename}" --stage2_path "all_checkpoints/pt_${filename}/last.ckpt" --opt_model $llm --max_epochs 100 --mode ft --tune_gnn --prompt 'The molecule\t' --init_lr 1e-4 --scheduler None --batch_size 16 --accumulate_grad_batches 2;
 exit
 }
