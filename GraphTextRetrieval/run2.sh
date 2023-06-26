803ba29e7d302ca2775c70d9d0fafcbaad746b3d
zyliu <acharkq@gmail.com>
2023-06-26 15:01:14 +0000

    huge change: refract structure

--- GraphTextRetrieval/run2.sh
+++
@@ -1,6 +0,0 @@
-{
-# source /gpu-work/gp9000/gp0900/next/anaconda3/etc/profile.d/conda.sh;
-# conda activate pth20;
-
-python main2.py --test_dataset ../GraphTextPretrain/data/PubChemDataset/PubChem-320k/test --init_checkpoint ../GraphTextPretrain/all_checkpoints/lsh/epoch=9-step=20290.ckpt --device 1 
-}
