b9b89c693e6a2e8ea5afe9a0856b3aef7780b88a
zyliu <acharkq@gmail.com>
2023-06-27 15:02:53 +0000

    update

--- scripts/stage1.sh
+++
@@ -1,8 +0,0 @@
-{
-
-python stage1.py --root 'data/PubChemDataset_v4' --gtm --lm --devices '6,7' --mode train --filename stage1_q32_tune_gnn --rerank_cand_num 128 --num_query_token 32
-
-# python stage1.py --root 'data/kv_data' --gtm --lm --devices '4,5'  --filename pcdes --init_checkpoint "all_checkpoints/stage1_default_tune_gnn/epoch=49-step=120950.ckpt" --rerank_cand_num 128 --num_query_token 8 --match_batch_size 64 --mode eval --use_phy_eval
-
-exit
-}
